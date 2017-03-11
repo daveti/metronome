f1e93961646d679359fad7f548da480a0dc3f3f5
Dave Tian <dave.jing.tian@gmail.com>
2017-03-11 17:39:56 -0500

    mbed client done

--- mbed-client-metronome/main.cpp
+++ mbed-client-metronome/main.cpp
@@ -20,15 +20,72 @@ DigitalOut g_led_blue(LED3);
 
 InterruptIn g_button_mode(SW3);
 InterruptIn g_button_tap(SW2);
+			
+
+metronome *g_met;
+bool g_play;
+bool g_learn;
+bool g_bpm;
+bool g_debug = false;
+bool g_debug2 = false;
+Mutex g_mutex;
+Semaphore g_sam(0);
+
+void dark()
+{
+	g_led_red = active_low::off;
+	g_led_green = active_low::off;
+	g_led_blue = active_low::off;
+}
+
+void bright()
+{
+	g_led_red = active_low::on;
+	g_led_green = active_low::on;
+	g_led_blue = active_low::on;
+}
 
 void on_mode()
 {
+    //dark();    
+    
     // Change modes
+    //dark();
+    if (g_met->is_timing()) {
+    	// Play mode
+		//g_met->stop_timing();
+		g_play = true;
+		g_learn = false;
+		g_bpm = true;
+		//if (g_debug2)
+			//utils::pulse(g_led_green);
+    } else {
+    	// Learn mode
+    	g_met->start_timing();
+    	g_play = false;
+    	//dark();
+    }
+    //g_sam.release();
 }
 
 void on_tap()
 {
     // Receive a tempo tap
+    if ((!g_met->is_timing()) || (g_play))
+    	return;
+    	
+    // Simplify the ISR
+    g_learn = true;
+    g_sam.release();
+    	
+    //dark();
+   
+   	// Save the sample
+   	// Q: shall we consider the delay caused by pulse? 	
+    //g_met->tap();
+    
+    // Q: can we do pulse here?
+	//utils::pulse(g_led_red);
 }
 
 int main()
@@ -36,12 +93,22 @@ int main()
 	// Seed the RNG for networking purposes
     unsigned seed = utils::entropy_seed();
     srand(seed);
+    
+    // Init met
+    g_met = new metronome();
+#ifdef IOT_ENABLED
+    // Init freq dev
+    g_met->init_freq_dev();
+#endif
 
 	// LEDs are active LOW - true/1 means off, false/0 means on
 	// Use the constants for easier reading
-    g_led_red = active_low::on;
-    g_led_green = active_low::on;
-    g_led_blue = active_low::on;
+    //g_led_red = active_low::on;
+    //g_led_green = active_low::on;
+    //g_led_blue = active_low::on;
+	g_led_red = active_low::off;
+	g_led_green = active_low::off;
+	g_led_blue = active_low::off;
 
 	// Button falling edge is on push (rising is on release)
     g_button_mode.fall(&on_mode);
@@ -49,7 +116,7 @@ int main()
 
 #ifdef IOT_ENABLED
 	// Turn on the blue LED until connected to the network
-    g_led_blue = active_low::off;
+    g_led_blue = active_low::on;
 
 	// Need to be connected with Ethernet cable for success
     EthernetInterface ethernet;
@@ -67,12 +134,15 @@ int main()
 
     M2MDevice* device = frdm_client::make_device();
     objects.push_back(device);
+    
+    // Add the freq dev
+    objects.push_back(g_met->get_device());
 
 	// Publish the RESTful endpoints
     client.connect(objects);
 
 	// Connect complete; turn off blue LED forever
-    g_led_blue = active_low::on;
+    g_led_blue = active_low::off;
 #endif
 
     while (true)
@@ -82,8 +152,64 @@ int main()
             break;
 #endif
 
+		if (g_debug2) {
+			utils::pulse(g_led_red);
+			wait(1);
+		}
+		
+		g_sam.wait(100);
+
         // Insert any code that must be run continuously here
+        //g_mutex.lock();
+        if (g_play) {
+        	// Stop timing
+        	g_met->stop_timing();
+        	
+        	// Compute the BPM here
+        	if (g_bpm) {
+        		g_bpm = false;
+        		g_met->get_bpm();
+#ifdef IOT_ENABLED
+				g_met->update_bpm_for_get_all();
+#endif
+			}
+        	
+        	//utils::pulse(g_led_green, g_met->get_freq());
+        	//utils::pulse(g_led_green, 1);
+        	//debug
+        	if (g_debug2) {
+				utils::pulse(g_led_blue);
+        	}
+        	if (g_debug) {
+        		if (g_met->get_freq() > 1) {
+        			g_led_blue = true;
+        			wait(0.5);
+        			g_led_blue = false;
+        			wait(0.5);
+        			continue;
+        		}
+        	}
+        	
+        	//g_led_green = true;
+        	//wait(g_met->get_freq()/2);
+        	//wait(0.5);
+        	//g_led_green = false;
+        	//wait(g_met->get_freq()/2);
+        	//wait(0.5);
+        	utils::pulse(g_led_green, g_met->get_freq());
+        } else if (g_learn) {
+        	 g_learn = false;
+        	 g_met->tap();
+        	 utils::pulse(g_led_red);
+        } else {
+        		dark();
+        		wait(0.2);
+        }
+        //g_mutex.unlock();
     }
+    
+    if (g_debug2)
+    	bright();
 
 #ifdef IOT_ENABLED
     client.disconnect();
--- mbed-client-metronome/metronome.hpp
+++ mbed-client-metronome/metronome.hpp
@@ -1,15 +1,24 @@
 #pragma once
 
 #include "mbed.h"
+#include "mbed-client/m2mdevice.h"
+#include "mbed-client/m2mobject.h"
+#include "mbed-client/m2minterface.h"
+#include "mbed-client/m2minterfacefactory.h"
 
-class metronome
+class metronome : public M2MInterfaceObserver
 {
 public:
     enum { beat_samples = 3 };
+    enum {
+    	BPM_CURRENT = 0,
+    	BPM_MIN = 1,
+    	BPM_MAX = 2
+	};
 
 public:
     metronome()
-    : m_timing(false), m_beat_count(0) {}
+    : m_timing(false), m_beat_count(0), m_bpm_current(0), m_bpm_min(0), m_bpm_max(0), m_freq_current(0.0) {}
     ~metronome() {}
 
 public:
@@ -23,9 +32,39 @@ public:
     void tap();
 
     bool is_timing() const { return m_timing; }
+    
 	// Calculate the BPM from the deltas between m_beats
 	// Return 0 if there are not enough samples
-    size_t get_bpm() const;
+    size_t get_bpm();
+    
+    // Set current bpm
+    void set_bpm(size_t bpm);
+    
+    // Reset min/max bpm
+    void reset_bpm();
+    
+    // For frdm_web
+    size_t get_bpm_current() const;
+    size_t get_bpm_min() const;
+    size_t get_bpm_max() const;
+    
+    // Calculate the frequency
+    float get_freq();
+    
+    // For connector
+    void init_freq_dev();
+    M2MObject* get_device();       
+    void value_updated(M2MBase *base, M2MBase::BaseType type);
+    void handle_bpm_reset(void *argument);
+    void update_bpm_for_get(int res);
+    void update_bpm_for_get_all();
+       
+    virtual void bootstrap_done(M2MSecurity* security) {}
+    virtual void object_registered(M2MSecurity*, const M2MServer&) {}
+    virtual void object_unregistered(M2MSecurity*) {}
+    virtual void registration_updated(M2MSecurity*, const M2MServer&) {}
+    //virtual void value_updated(M2MBase*, M2MBase::BaseType) {}
+    virtual void error(M2MInterface::Error error) {}
 
 private:
     bool m_timing;
@@ -34,4 +73,16 @@ private:
 	// Insert new samples at the end of the array, removing the oldest
     size_t m_beats[beat_samples];
     size_t m_beat_count;
+    float m_beats2[beat_samples];
+    volatile size_t m_bpm_current;
+    size_t m_bpm_min;
+    size_t m_bpm_max;
+    float m_freq_current;
+    
+    // For connector
+    M2MObject*  freq_obj;
+    Mutex		bpm_mutex;
+    //M2MResource* freq_bpm_current_res;
+    //M2MResource* freq_bpm_min_res;
+    //M2MResource* freq_bpm_max_res;
 };
